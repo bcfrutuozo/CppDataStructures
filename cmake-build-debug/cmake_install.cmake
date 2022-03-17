a17506e9b2de86cfba7d7cc0017ad4e634345b08
bcfrutuozo <tI%fYhZDCVak@1X1>
2022-03-17 15:52:38 -0300

    Creating unit test for Array<T> class.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /data/bcfrutuozo/source/lnxcpplearning
+# Install script for directory: /data/bcfrutuozo/source/CppDataStructures
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,6 +42,16 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/data/bcfrutuozo/source/CppDataStructures/cmake-build-debug/dependencies/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/data/bcfrutuozo/source/CppDataStructures/cmake-build-debug/tests/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -50,5 +60,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/data/bcfrutuozo/source/lnxcpplearning/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/data/bcfrutuozo/source/CppDataStructures/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -3,21 +3,11 @@
 #include <vector>
 #include <iostream>
 
-void Test() {
-
-    Array<int> a = { 0, 1, 2, 3, 4, 5};
-    Array<int> b = { 5, 4, 3, 2, 1, 0};
-
-    Array<int>::Copy(a, 0, b, 0);
-
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-}
-
 int main(int argc, char *argv[]) {
-    Test();
 
     std::vector<int> z = { 1, 2, 3, 4, 5};
+
+    std::cout << z[0];
 
     return 0;
 }
